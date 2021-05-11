a946fcb7e574325aa6464eb63ee0492043406842
WebExpertEver <webexpert.michael@gmail.com>
2021-05-11 18:04:40 +0900

    Back-end final version

--- src/app/store/effects/index.ts
+++
@@ -1,7 +0,0 @@
-import { RouterEffects } from './router.effect';
-import { AuthEffects } from './auth.effect';
-
-export const effects: any[] = [RouterEffects, AuthEffects];
-
-export * from './router.effect';
-export * from './auth.effect';
--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    hmr       : false,
-    backendBaseUrl: 'http://localhost:8000',
-    oAuthClientID: 2,
-    oAuthClientSecret: 'ExZMftTCvXqCDtWEXCRZrEX7hDdN4F8wg7GIkgJF'
-};
